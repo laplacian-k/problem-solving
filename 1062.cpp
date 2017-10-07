f857ed816bc59faa4bfdb37e6f234a16c96225f1
James Park <laplacian.k@gmail.com>
2017-10-07 20:18:54 +0900

    Update 1062.cpp

--- 1062.cpp
+++ 1062.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 // variables
@@ -7,12 +8,35 @@ int N, K;
 string word[51];
 
 // functions
-int solve(int learn)
+// learn집합으로 읽을 수 있는 단어의 최대 수
+int solve(int learn, int cnt)
 {
     // 배운 알파벳 수가 K를 넘어가면 안됌.
-    if(>K) return 0;
+    if(cnt > K) {
+        int ret = 0;
+        for(int i = 0; i < N; i++) {
+            bool canMake = true;
+            for(int j = 0; j < word[i].length(); j++) {
+                if( (learn & (1<<(word[i][j]-'a'))) != 1 ) {
+                    canMake = false;
+                    break;
+                }
+            }
+            if(canMake)
+                cnt++;
+        }
+        return cnt;
+    }
+    
+    int ret = 0;
     // 배우는 알파벳의 조합을 만듦.
-    for(int i = 0; i < 26; i++)
+    for(int i = 0; i < 26; i++) {
+        // 이미 배웠으면 skip
+        if((learn & (1>>i)) == 1) continue;
+        ret = max(ret, solve((learn+(1>>i)), cnt+1));
+    }
+    
+    return ret;
 }
 
 int main()
@@ -24,11 +48,10 @@ int main()
         cin >> word[i];
     }
     
+    
     // algorithm
     // antatica -> a, n, t, i, c 5글자는 반드시 알아야 함.
-    int ret = 0;
-    if(K < 5) ret = 0;// 정답 0 리턴
-    if(K == 26) ret = N;
+    int learn = (1>>0) + (1>>2) + (1>11) + (1>>14) + (1>>20);
     // 5 + antic 이외의 글자 집합의 개수 > K라면 그 단어는 배울 수 없음.
     
     // 2차원 배열로 만들어놓고 알파벳 별로 필요한 개수를 카운트. 가장 필요한 단어부터 배운다? x
@@ -36,15 +59,18 @@ int main()
     // 나머지 1개의 단어에 필요한 {t}를 배워야 단어를 읽을 수 있음.
     
     // K에 맞게 알파벳 집합을 선택하고 모든 단어에 대해 가능한지 탐색하면서 최대값을 구해봄.
-    // O(21C(k-5)*50) 시간 초과가 예상됨.
-    // 배우는 알파벳의 집합을 비트마스크로 표현하고 메모이제이션 사용!
-    // 캐시사이즈 : 2^26-1 = 10^6*64 = 64000000. 약 64mb
-    // O(N)시간이 예상됨!
+    // O(21C(k-5)*50) 시간 초과? x
+    // O(2^(k-5)*50) =10^7을 좀 더 넘는 시간! 
     
     // abcdefghijklmnopqrstuvwxyz
     // 10100000001001000001000000 -> reversed
-    int learn = (1>>0) + (1>>2) + (1>11) + (1>>14) + (1>>20);
-    ret = solve(learn);
+    int ret = 0;
+    if(K < 5)
+        ret = 0;// 정답 0 리턴
+    else if(K == 26)
+        ret = N;
+    else
+        ret = solve(learn, 5);
         
     cout << ret << endl;
     

c489e64229a80d2a70e4ccad24822b62460b3342
James Park <laplacian.k@gmail.com>
2017-11-16 12:36:52 +0900

    Create 1670.cpp

---
+++ 1670.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// variables
+int N;
+const int MOD = 987654321;
+
+// functions
+
+int main()
+{
+    // input
+    cin >> N;
+    
+    // algorithm
+    int ret = 0;
+    cout << ret << endl;
+    
+    return 0;
+}

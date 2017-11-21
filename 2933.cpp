a4e99eb05fad6f30f1ef25d73ef6813b27b1068c
James Park <laplacian.k@gmail.com>
2017-11-21 12:41:48 +0900

    Update 2933.cpp

--- 2933.cpp
+++ 2933.cpp
@@ -9,7 +9,31 @@ int N;
 int h[100];
 
 // functions
-
+void destroy(int i)
+{
+    // 짝수면 왼->오, 홀수면 오->왼        
+    
+    int high = h[i]-1;
+    // 홀수
+    if(i&1){
+        for(int k = C-1; k >= 0; k--) {
+            if(cave[high][k] == 'x'){
+                cave[high][k] = '.';
+                break;
+            }
+        }
+    }
+    else {
+        for(int k = 0; k < C; k++) {
+            if(cave[high][k] == 'x'){
+                cave[high][k] = '.';
+                break;
+            }
+        }        
+    }
+    
+    return;
+}
 int main()
 {
     // input
@@ -30,6 +54,15 @@ int main()
     // 막대를 던질 높이를 정하면 막대는 땅과 수평으로 날라감 (턴제 게임임. 왼쪽에서 오른쪽부터 던짐)
     // 클러스터의 정의? 인접하면 됨. 중력에 의해서 밑으로 떨어짐.
     // 클러스터는 다른 클러스터나 땅을 만나기 전까지 게속해서 떨어진다(클러스터를 인식하고 그 클러스터의 밑바닥에 무언가 닿아야함)
+    
+    for(int i = 0; i < N; i++) {
+        // 짝수면 왼->오, 홀수면 오->왼        
+        // 미네랄 파괴
+        destroy(i);
+        // 클러스터 검증
+        
+        // 동굴업데이트
+    }
     
     // 변경된 동굴의 모습이 출력됨.
     for(int i = 0; i < R; i++) {

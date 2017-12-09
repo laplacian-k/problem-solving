cdf36796aea60b90ba54e01a59bf381785dedc36
James Park <laplacian.k@gmail.com>
2017-12-10 02:17:02 +0900

    Update 11058-1.cpp

--- 11058-1.cpp
+++ 11058-1.cpp
@@ -15,39 +15,36 @@ long long n;
 // d[n][1] = max(d[n+1][0]+1, d[n+1][1]+buffer) (if possible index)
 long long kriii(int d, int state)
 {
-    cout << d << ", " << state << ", " << buffer << ", " << n << endl;
+    //cout << d << ", " << state << ", " << buffer << ", " << n << endl;
     //cout << d << ", " << state << endl;
     long long& ans = cache[d][state];
     if(ans != -1) return ans;
    
-    if(d < N)
+    if(d >= N) {
+        return 0;
+    }
     long long ret = 0;
     
     // 버퍼 유무에 상관없이 A를 쓸 수 있다.
     n += 1;
-    ret = max(ret, kriii(d+1, 0)+1);    // 버퍼를 비워버림.
+    ret = max(ret, kriii(d+1, state)+1);
     n -= 1;
     
     // 버퍼가 없다면
     if(state == 0) {
-        if(d+2 < N) {
-            int temp = buffer;
-            buffer = n;
-            // buffer 올리기
-            ret = max(ret, kriii(d+2, 1));
-            buffer = temp;            
-        }
+        long long temp = buffer;
+        buffer = n;
+        // buffer 올리기
+        ret = max(ret, kriii(d+2, 1));
+        buffer = temp;      
 
     }
     // 버퍼가 있다면
     else {
-        if(d+1 < N) {
-            // 붙여넣기
-            n += buffer;
-            ret = max(ret, kriii(d+1, 1)+buffer);
-            n -= buffer;            
-        }
-
+        // 붙여넣기
+        n += buffer;
+        ret = max(ret, kriii(d+1, 1)+buffer);
+        n -= buffer;    
     }
     
     return ans = ret;

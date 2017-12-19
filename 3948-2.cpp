3d7744e2c399f698b065a06ad351b74efdaba199
James Park <laplacian.k@gmail.com>
2017-12-19 12:48:36 +0900

    Update 3948-2.cpp

--- 3948-2.cpp
+++ 3948-2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // variables
 int N;
 long long u[21];
-long long c[21][21];
+long long c[22][22];
 
 int main()
 {
@@ -15,22 +15,21 @@ int main()
     // pre processing
     // nCr = n-1Cr-1 + n-1Cr
     c[0][0] = 1;
-    //c[0][1] = 0;
-    //c[1][0] = 1;
     
     for(int i = 1; i <= 20; i++) {
-    	c[i][0] = 1;
-        for(int j = 1; j <= i; j++) {
+    	c[i][0] = c[i][i] = 1;
+        for(int j = 1; j <= i-1; j++) {
             c[i][j] = c[i-1][j-1]+c[i-1][j];
         }
     }
-    
+    /*
     for(int i = 0; i <= 20; i++) {
         for(int j = 0; j <= 20; j++) {
             cout << c[i][j] << " ";
         }
         cout << endl;
     }
+    */
     
     while(T--) {
         // input2
@@ -43,16 +42,17 @@ int main()
         // 가장 큰 병사를 기준(j번째에 위치)으로 왼쪽과 오른쪽으로 나눌 수 있음
         // u[j-1]*u[i-j]*왼쪽에 위치할 병사를 고르는 경우의 수 = u[j-1]*u[i-j]*i-1Cj-1 (0<=j=<i j는 짝수)
         
-        u[0] = u[1] = 1;
+        u[0] = u[1] = u[2] = 1;
         
-        for(int i = 2; i <= N; i++) {
-            for(int j = 2; j <= i; j++) {
+        for(int i = 3; i <= N; i++) {
+            for(int j = 1; j <= i; j+=2) {
                 // 홀수라면 skip
-                if(j^1) continue;
-                u[i] = u[j-1]*u[i-j]*c[i-1][j-1];
+                //if(j&1) continue;
+                u[i] += u[j-1]*u[i-j]*c[i-1][j-1];
             }
         }
         
+        /*
         cout << "u[i] 출력" << endl;
         for(int i = 0; i <= N; i++) {
         	cout << u[i] << " ";
@@ -60,6 +60,7 @@ int main()
         cout << endl;
         
         cout << "정답출력" << endl;
+        */
         long long ret = 0;
         if(N == 1) ret = 1;
         else {

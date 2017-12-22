ca9cdb0595fdb5e3ca9c8be7d9ae3fabf3c7adb0
James Park <laplacian.k@gmail.com>
2017-12-22 13:35:43 +0900

    Create 2698-1.cpp

---
+++ 2698-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// variables
+int n, k;
+
+//functions
+
+int main()
+{
+    int T;
+    cin >> T;
+    
+    while(T--) {
+        // input
+        cin >> n >> k;
+        
+        // output
+        //cout << n << " " << k << endl;
+        
+        // algorithm
+        // 인접한 비트의 개수가 k인 수열 S의 개수를 출력
+        int ret = 0;
+        cout << ret << endl;
+    }
+    
+    return 0;
+}

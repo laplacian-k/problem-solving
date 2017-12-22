9243895dae424bc46f12c615923afff70902f5be
James Park <laplacian.k@gmail.com>
2017-12-22 18:35:43 +0900

    Update 2698-1.cpp

--- 2698-1.cpp
+++ 2698-1.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 // variables
-int n, k;
+int N, K;
+int d[101][101][2];
 
 //functions
 
@@ -11,17 +12,34 @@ int main()
     int T;
     cin >> T;
     
+    // algorithm
+    // 인접한 비트의 개수가 k인 수열 S의 개수를 출력
+    // a[n][k] : n자리 비트 중 인접비트가 k개인 수열 S의 개수
+    // a[n][k] = d[n][k][1] + d[n][k][0] (끝자리가 1, 끝자리가 0)
+    // d[n][k][1] = d[n-1][k-1][1] + d[n-1][k][0]
+    // d[n][k][0] = d[n-1][k][1] + d[n-1][k][0]
+
+    d[1][0][0] = d[1][0][1] = 1;
+
+    for(int n = 2; n <= 100; n++) {
+        for(int k = 0; k < n; k++) {
+            d[n][k][1] = d[n-1][k][0];
+            d[n][k][0] = d[n-1][k][1] + d[n-1][k][0];
+            if(k-1 >= 0) {
+                d[n][k][1] += d[n-1][k-1][1];
+            }
+        }
+    }
+    
     while(T--) {
         // input
-        cin >> n >> k;
+        cin >> N >> K;
         
         // output
-        //cout << n << " " << k << endl;
+        //cout << N << " " << K << endl;
         
-        // algorithm
-        // 인접한 비트의 개수가 k인 수열 S의 개수를 출력
-        int ret = 0;
-        cout << ret << endl;
+
+        cout << d[N][K][1] + d[N][K][0] << endl;
     }
     
     return 0;

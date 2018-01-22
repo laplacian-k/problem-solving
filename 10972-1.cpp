1fd286ec154aeba4f010c4a63fceaa009259f9e5
James Park <laplacian.k@gmail.com>
2018-01-22 12:30:49 +0900

    next_permutation

---
+++ 10972-1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>    // for next_permutation
+using namespace std;
+
+// variables
+int N;
+
+int main()
+{
+    // input
+    cin >> N;
+    vector<int> a(N);
+    for(int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+    
+    // algorithm
+    if(next_permutation(a.begin(), a.end())) {
+        for(int i = 0; i < N; i++) {
+            cout << a[i] << ' ';    
+        }        
+    }
+    else {
+        cout << -1 << endl;
+    }
+    cout << endl;
+    
+    return 0;
+}

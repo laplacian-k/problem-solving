4093d497874a5be27b24c52d21a2bafb5e5d23e1
James Park <laplacian.k@gmail.com>
2018-01-22 15:44:21 +0900

    Update 10974-2.cpp

--- 10974-2.cpp
+++ 10974-2.cpp
@@ -8,27 +8,24 @@ int N;
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
     // input
     cin >> N;
     
-    vector<int> p;
+    vector<int> p(N);
     
     // algorithm
-    for(int i = 1; i <= N; i++) {
-        p.push_back(i);
-    }
     for(int i = 0; i < N; i++) {
+        p[i] = i+1;
         cout << p[i] << ' ';
-    } 
-    cout << endl;
+    }
+    cout << '\n';
     
     
     while(next_permutation(p.begin(), p.end())) {
         for(int i = 0; i < N; i++) {
             cout << p[i] << ' ';
         }
-        cout << endl;
+        cout << '\n';
     }
     
     

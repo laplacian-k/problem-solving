214ea5cbaa3e3c77e83a1e2d3706aaf69557ed26
James Park <laplacian.k@gmail.com>
2018-05-23 23:56:50 +0900

    Update topcoder_a_new_hope.cpp

--- topcoder_a_new_hope.cpp
+++ topcoder_a_new_hope.cpp
@@ -8,33 +8,48 @@ public:
 };
     
 int ANewHope::count(vector<int> firstWeek, vector<int> lastWeek, int D) {
-    int N = firstWeek.size();
-    vector<int> idxs(2501, -1);
     
+    int N = firstWeek.size();
+    // validation
+    bool isEqual = true;
+    for(int i = 0; i < N; i++) {
+        if(firstWeek[i] != lastWeek[i]) {
+            isEqual = false;
+            break;
+        }
+    }
+    
+    if(isEqual) return 1;            
+    
+    // init last week index
+    vector<int> idxs(2501, -1);    
     for(int i = 0; i < N; i++) {
         idxs[lastWeek[i]] = i;
     }
     
-    int ret = 0;
-    vector<int> conv = firstWeek;
+    int ret = 2;
+    
     while(1) {
-        for(int i = 0; i < N; i++) {
-            conv[(i+D)%N] = firstWeek[i];
-        }
         
-        firstWeek = conv;
+        bool canMake = true;       
+        vector<int> mid(N);
         
-        bool canMake = true;
         for(int i = 0; i < N; i++) {
-            if(i+D < N) continue;
-            if((i+D)%N > idxs[conv[i]]) {
+            if(i+D < N) {
+                mid[i+D] = firstWeek[i];
+                continue;
+            }            
+            
+            if((i+D)%N > idxs[firstWeek[i]]) {
                 canMake = false;
-                break;
             }
+            
+            mid[(i+D)%N] = firstWeek[i];
         }
         
         if(canMake) break;
         
+        firstWeek = mid;
         ret += 1;
     }
     
